7fff29d8567ecf95ef5dd41065bb3c814db1ce64
Fishstick327 <nathan07tran@gmail.com>
2022-11-25 01:26:52 -0800

    Add files via upload

---
+++ Week 3/js/js-hello-world.js
@@ -0,0 +1,8 @@
+function sayHello(name) {            
+    alert('Hello ' + name + '!');
+    console.log('Alert Sent!');
+    }
+    function getName () {
+        const userName = prompt('what is your name?');
+        sayName(userName);
+    }
